8b76c2d23cb79337e0a2366df5cd9240e1976f96
Arpit Singh <kailashsinghparmarrajput@gmail.com>
2022-06-19 10:24:41 +0530

    hlo

---
+++ lua/ncm.lua
@@ -0,0 +1,125 @@
+local cmp = require'cmp'
+local lspkind = require('lspkind')
+
+local icons = {
+	Namespace = "",
+   Text = " ",
+   Method = " ",
+   Function = " ",
+   Constructor = " ",
+   Field = "ﰠ ",
+   Variable = " ",
+   Class = "ﴯ ",
+   Interface = " ",
+   Module = " ",
+   Property = "ﰠ ",
+   Unit = "塞 ",
+   Value = " ",
+   Enum = " ",
+   Keyword = " ",
+   Snippet = " ",
+   Color = " ",
+   File = " ",
+   Reference = " ",
+   Folder = " ",
+   EnumMember = " ",
+   Constant = " ",
+   Struct = "פּ ",
+   Event = " ",
+   Operator = " ",
+   TypeParameter = " ",
+   Table = "",
+   Object = " ",
+   Tag = "",
+   Array = "[]",
+   Boolean = " ",
+   Number = " ",
+   Null = "ﳠ",
+   String = " ",
+   Calendar = "",
+   Watch = " ",
+   Package = "",
+}
+
+local function border(hl_name)
+   return {
+      { "╭", hl_name },
+      { "─", hl_name },
+      { "╮", hl_name },
+      { "│", hl_name },
+      { "╯", hl_name },
+      { "─", hl_name },
+      { "╰", hl_name },
+      { "│", hl_name },
+   }
+end
+  cmp.setup({
+	  formatting = {
+        format = function(_,vim_item)
+			vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+			return vim_item
+		end,
+    },
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      completion = {
+         border = border "CmpBorder",
+         winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+      },
+      documentation = {
+         border = border "CmpDocBorder",
+      },
+   },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+    --  { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
